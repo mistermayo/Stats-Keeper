c03a49c63a7dc6262bcce739f2e7d66c20256484
tucker <tuckerjamesmayo@gmail.com>
2015-03-10 12:04:29 -0700

    refactor models, add application, team, teams and player templates

--- models/player.js
+++ models/player.js
@@ -1,5 +1,9 @@
 Stat.Player = DS.Model.extend({
   name: DS.attr(),
+  shot: null,
+  shot: function() {
+  return this.get('player') + '; shot: ' + this.get('shot');
+  }.property('player', 'shot');
   team: DS.belongsTo('team', {async: true})
   shotAttempts: DS.hasMany('shotAttempts', {async: true})
 });
--- models/shotAttempt.js
+++ models/shotAttempt.js
@@ -1,10 +1,6 @@
 Stat.shotAttempt = DS.Model.extend({
 score: null;
 shot: null;
-
-shotAttempt: function() {
-  return this.get('player') + '; shot: ' + this.get('shot');
-}.property('player', 'shot');
 player: DS.belongsTo('player', {async: true})
 
 });
